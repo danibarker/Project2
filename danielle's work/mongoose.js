f4f9c5b8d6bcb5bb609dc060c9f9b9557a357d72
danibarker <caseybarker86@gmail.com>
2020-10-27 23:17:10 -0600

    cleaning up repository for Fernando's init commit

--- danielle's work/mongoose.js
+++ danielle's work/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+const { env } = require("process");
 const connectionURL = 'mongodb+srv://admin:KDCYfjlXj8Qg0eTp@cluster0.wpqc6.mongodb.net/media-warnings?retryWrites=true&w=majority';
 mongoose.connect(connectionURL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
 });
+
+process.env.connectionURL
