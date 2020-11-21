5a05de89c3cfe67bd4b5f2114a1eaddcb2035bc0
Greg Fenton <greg.fenton@gmail.com>
2020-11-21 11:53:32 -0700

    added userCreateIndex and body-parser

--- server/src/mwdb-Bret/app.js
+++ server/src/mwdb-Bret/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
 
@@ -10,8 +11,13 @@ const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1:27017/';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+// 'mwdb' below is name of database to use
+const mongoDB = 'mongodb://127.0.0.1:27017/mwdb';
+mongoose.connect(mongoDB, { 
+  useNewUrlParser: true , 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -24,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
