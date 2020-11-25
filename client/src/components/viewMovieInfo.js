fbfb7a45cd5819fcab6ca625ff56d3854b50656c
Danielle Barker <caseybarker86@gmail.com>
2020-11-25 15:28:57 -0700

    building JS functions to display totals for each warning type

--- client/src/components/viewMovieInfo.js
+++ client/src/components/viewMovieInfo.js
@@ -1,58 +1,114 @@
 import React from "react";
 import "../css/App.css";
-
-export default class viewMovieInfo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { none: "none" };
-    }
-    render() {
-        const movieTitle = "The Lion King";
-        const movieWarnings = [
-            {
-                category: "Animal Death",
-                type: "Explicit",
-                severity: "Mild",
-                frequency: "Sporadic",
-            },
-            {
-                category: "Cartoon Violence",
-                type: "Explicit",
-                severity: "Mild",
-                frequency: "Common",
-            },
-        ];
-        const warningsListItems = movieWarnings.map((warning) => {
-            return (
-                <li>
-                    <div>
-                        <strong>{warning.category}:</strong>
-                    </div>
-                    <div>
-                        Type:
-                        <span class="red">{warning.type}</span>
-                    </div>
-                    <div>
-                        Severity:
-                        <span class="yellow">{warning.severity}</span>
-                    </div>
-                    <div>
-                        Frequency:
-                        <span class="green">{warning.frequency}</span>
-                    </div>
-                    <div>
-                        <button>View user comments</button>
-                    </div>
-                </li>
-            );
+async function getWarnings(movieID) {
+    try {
+        const response = await fetch("/warnings/movieID=" + movieID, {
+            method: "GET",
         });
-        return (
-            <div id="notebooks">
-                <h2>{movieTitle}</h2>
-                <h3>Warnings:</h3>
-                <ul>{warningsListItems}</ul>
-                <button>Add warning</button>
-            </div>
-        );
+        const result = await response.json();
+
+        return result
+
+    } catch (e) {
+        console.log(e);
     }
 }
+export default function ViewMovieInfo(movieID) {
+    //const movieWarnings = getWarnings(movieID)
+    const movieTitle = "The Lion King";
+    const movieWarnings = [
+        {
+            category: "Animal Death",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 1,
+            severity: 2,
+            frequency: 2,
+        },
+        {
+            category: "Cartoon Violence",
+            type: 2,
+            severity: 2,
+            frequency: 2,
+        },
+    ]
+    let foundCategories = []
+    let movieWarningsF = movieWarnings.reduce((warnings, warning) => {
+        if (!foundCategories.includes(warning.category)) {
+            foundCategories.push(warning.category)
+            warnings[warning.category] = warning
+            warnings[warning.category].count = 1
+            return warnings
+        } else {
+            warnings[warning.category].type += warning.type
+            warnings[warning.category].severity += warning.severity
+            warnings[warning.category].frequency += warning.frequency
+            warnings[warning.category].count ++
+            return warnings
+        }
+    }, {})
+
+
+    console.log(movieWarningsF)
+    const warningsListItems = Object.keys(movieWarningsF).map((warning) => {
+        return (
+            <li>
+                <div>
+                    <strong>{movieWarningsF[warning].category}:</strong>
+                </div>
+                <div>
+                    Type:
+                    <span className="red">{movieWarningsF[warning].type}</span>
+                </div>
+                <div>
+                    Severity:
+                    <span className="yellow">{movieWarningsF.[warning].severity}</span>
+                </div>
+                <div>
+                    Frequency:
+                    <span className="green">{movieWarningsF.[warning].frequency}</span>
+                </div>
+                <div>
+                    <button>View user comments</button>
+                </div>
+            </li>
+        );
+    });
+    return (
+        <div id="notebooks">
+            <h2>{movieTitle}</h2>
+            <h3>Warnings:</h3>
+            <ul>{warningsListItems}</ul>
+            <button>Add warning</button>
+        </div>
+    );
+}
+
