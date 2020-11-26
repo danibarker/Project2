d357874b17d66beb12be47455c3117201ba6e4c1
Greg Fenton <greg.fenton@gmail.com>
2020-11-26 14:54:49 -0700

    initial commit, required by server.js

---
+++ server/src/app.js
@@ -0,0 +1,61 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const debug = require('debug')('mwdb:server');
+
+const indexRouter = require('./routers/index');
+const adminRouter = require("./routers/admin")
+//const tmdbRouter = require("./routers/tmdb")
+
+const app = express();
+
+//Set up mongoose connection
+debug('making mongoose connection')
+const mongoose = require('mongoose');
+// 'mwdb' below is name of database to use
+const mongoDB = 'mongodb://127.0.0.1:27017/mwdb';
+mongoose.connect(mongoDB, { 
+  useNewUrlParser: true , 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', indexRouter);
+app.use("/admin", adminRouter)
+//app.use("/tmdb", tmdbRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
