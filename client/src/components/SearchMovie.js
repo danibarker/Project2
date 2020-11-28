0a390f56a8d079fa9408ba8c9562443bcb9bc403
danibarker <caseybarker86@gmail.com>
2020-11-27 18:08:48 -0700

    trying to fix something

--- client/src/components/SearchMovie.js
+++ client/src/components/SearchMovie.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/App.css";
 import  { useState } from 'react';
 
-const serverURL = process.env.PORT ? '' : 'http://localhost:3000'
+const serverURL = process.env.DATABASE_URL ? '' : 'http://localhost:3000'
 
 
 
