63134382ab96f2cb6cedafcf671321a1c30652d2
Fernando Cerda <f_cerda@hotmail.com>
2020-12-03 10:18:52 -0700

    Add ty, Frequency and Severity

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -4,12 +4,16 @@ import { getAllResource } from "../requests/gets";
 
 
 export default ({ setCurrentPage }) => {
-  const [adminType, setAdminType] = useState("Category");
+  const [adminType, setAdminType] = useState("Category", "Frequency", "Type", "Severity");
   let pageToShow;
   if (adminType == 'Category') {
     pageToShow = <Category />
-  } else if (adminType == 'Frequency') {
+  }  else if (adminType == 'Frequency') {
     pageToShow = <Frequency />
+  } else if (adminType == 'Type') {
+    pageToShow = <Type />
+  } else if (adminType == 'Severity') {
+    pageToShow = <Severity />
   }
   return (
     <div>
@@ -20,14 +24,16 @@ export default ({ setCurrentPage }) => {
       >
         <option>Category</option>
         <option>Frequency</option>
+        <option>Type</option>
+        <option>Severity</option>
       </select>
       {pageToShow}
     </div>
   );
 };
-const Frequency = () => {
-  return <h1>Frequency</h1>
-}
+// const Frequency = () => {
+//   return <h1>Frequency</h1>
+// }
 
 const Category = () => {
   const [categories, setCategories] = useState();
@@ -75,4 +81,170 @@ const Category = () => {
       </div>
     </div>
   );
+
+
+};
+
+
+
+
+
+
+
+
+// Adding Frequencies
+
+const Frequency = () => {
+  const [frequencies, setFrequencies] = useState();
+  const [newFrequency, setNewFrequency] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      await getPageData();
+    };
+    getData();
+  }, []);
+
+  const getPageData = async () => {
+    const currentFrequencies = await getAllResource("frequencies");
+    setFrequencies(currentFrequencies);
+  };
+
+  const createNewFrequency = async () => {
+    await addResource("frequency", { title: newFrequency, value:newFrequency});
+    setTimeout(() => {
+      getPageData();
+    }, 2000);
+  };
+  if (!frequencies) {
+    return <h1>Loading, please wait....</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        <div>
+          {frequencies.map((frequency) => (
+            <li>{frequency.title}</li>
+          ))}
+        </div>
+      </ul>
+      <div>
+        <h1>Frequency</h1>
+        <h2>Add</h2>
+        <input
+          onChange={(e) => {
+            setNewFrequency(e.target.value);
+          }}
+          placeholder="New Frequency Title"
+        />
+        <button onClick={createNewFrequency}>Submit</button>
+      </div>
+    </div>
+  );
+
+
+};
+
+// Adding Type
+
+const Type = () => {
+  const [types, setTypes] = useState();
+  const [newType, setNewType] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      await getPageData();
+    };
+    getData();
+  }, []);
+
+  const getPageData = async () => {
+    const currentTypes = await getAllResource("types");
+    setTypes(currentTypes);
+  };
+
+  const createNewType = async () => {
+    await addResource("type", { title: newType});
+    setTimeout(() => {
+      getPageData();
+    }, 2000);
+  };
+  if (!types) {
+    return <h1>Loading, please wait....</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        <div>
+          {types.map((type) => (
+            <li>{type.title}</li>
+          ))}
+        </div>
+      </ul>
+      <div>
+        <h1>Type</h1>
+        <h2>Add</h2>
+        <input
+          onChange={(e) => {
+            setNewType(e.target.value);
+          }}
+          placeholder="New Frequency Title"
+        />
+        <button onClick={createNewType}>Submit</button>
+      </div>
+    </div>
+  );
+
+
+};
+
+// Adding Severity
+
+
+const Severity = () => {
+  const [severities, setSeverities] = useState();
+  const [newSeverity, setNewSeverity] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      await getPageData();
+    };
+    getData();
+  }, []);
+
+  const getPageData = async () => {
+    const currentSeverities = await getAllResource("severities");
+    setSeverities(currentSeverities);
+  };
+
+  const createNewSeverity = async () => {
+    await addResource("severity", { title: newSeverity});
+    setTimeout(() => {
+      getPageData();
+    }, 2000);
+  };
+  if (!severities) {
+    return <h1>Loading, please wait....</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        <div>
+          {severities.map((severity) => (
+            <li>{severity.title}</li>
+          ))}
+        </div>
+      </ul>
+      <div>
+        <h1>Severity</h1>
+        <h2>Add</h2>
+        <input
+          onChange={(e) => {
+            setNewSeverity(e.target.value);
+          }}
+          placeholder="New Severity Title"
+        />
+        <button onClick={createNewSeverity}>Submit</button>
+      </div>
+    </div>
+  );
+
+
 };
