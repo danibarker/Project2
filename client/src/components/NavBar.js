798b2302bd62d7f24e18cf9e04e4028a5c4f0750
danibarker <caseybarker86@gmail.com>
2020-12-07 12:28:56 -0700

    changed sign in to sign in and sign up removed button styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,14 +4,20 @@ import Box from "@material-ui/core/Box";
 import logo from "../images/home.png";
 import titleImage from "../images/title.png";
 export default function NavBar({ currentPage, setCurrentPage }) {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   return (
     <>
       <div className="navBar">
         <Box display="flex" p={1} bgcolor="white">
-          <div style={{width: "10%"}}>
+          <div style={{ width: "10%" }}>
             <img
-              style={{ cursor: "pointer",width: "90%",margin: "0px 5%", maxHeight: "35px",maxWidth: "35px"}}
+              style={{
+                cursor: "pointer",
+                width: "90%",
+                margin: "0px 5%",
+                maxHeight: "35px",
+                maxWidth: "35px",
+              }}
               width="20px"
               src={logo}
               p={1}
@@ -20,26 +26,60 @@ export default function NavBar({ currentPage, setCurrentPage }) {
               }}
             ></img>
           </div>
-          <div style={{display: "flex",justifyContent: "center",width: "75%",margin: "auto"}}>
-            <img alt="The Movie Warning Database"
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "70%",
+              margin: "auto",
+            }}
+          >
+            <img
+              alt="The Movie Warning Database"
               src={titleImage}
-              style={{ width: "90%", height: "auto", margin:"0 5%", maxHeight: "35",maxWidth: "400px" }}
+              style={{
+                width: "90%",
+                height: "auto",
+                margin: "0 5%",
+                maxHeight: "35",
+                maxWidth: "400px",
+              }}
             ></img>
           </div>
-          <div style={{width:"10%",
-    
-    display: "flex",
-    justifyContent: "flex-end" }}>
-            <button
-              
-            className="btn btn-success" style={{ fontSize: "1.5vw", padding:" 0px 5px"}}
-                onClick={() => {
-                  setCurrentPage("SignIn");
-                }}
-              >
-              {token ? "My Account" : "Sign In"}
-              </button>
-            
+          <div
+            style={{
+              width: "20%",
+
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <span
+              className=""
+              style={{
+                cursor: "pointer",
+                fontSize: "1.5vw",
+                padding: " 0px 5px",
+              }}
+              onClick={() => {
+                setCurrentPage("SignIn");
+              }}
+            >
+              {token ? "My Account" : "Log In"}
+            </span>
+            <span
+              className=""
+              style={{
+                cursor: "pointer",
+                fontSize: "1.5vw",
+                padding: " 0px 5px",
+              }}
+              onClick={() => {
+                setCurrentPage("SignUp");
+              }}
+            >
+              {token ? "" : "Sign Up"}
+            </span>
           </div>
         </Box>
         <div>{currentPage}</div>
