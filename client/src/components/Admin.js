ffa8d2a555f2a76643d842516e046d138023dd54
danibarker <caseybarker86@gmail.com>
2020-12-09 11:14:22 -0700

    fixed == that should be ===

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -11,7 +11,7 @@ export default () => {
   );
   let pageToShow;
   const token = window.sessionStorage.getItem('token')
-  if (adminType == "Category") {
+  if (adminType === "Category") {
     pageToShow = <Category token={token}/>;
   } else if (adminType === "Frequency") {
     pageToShow = <Frequency token={token}/>;
