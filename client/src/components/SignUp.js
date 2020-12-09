69d22915bf5c11bba0c08b0656511e5966463df3
Bret Robinson <kakadu.gm@gmail.com>
2020-12-08 20:27:53 -0700

    adds a new user and error handling for dup username or email

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/App.css";
+import {Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, Row} from "reactstrap";
+import { addUser } from "../requests/posts";
 
-function SignUp() {
+export default function SignUp({setCurrentPage}) {
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [registerFail, setRegisterFail]= useState(false)
+
+  const changeUsername = (username) => {
+    setUsername( username );
+  }
+  const changeEmail = (email) => {
+    setEmail( email );
+  }
+  const changePassword = (password) => {
+    setPassword( password );
+  }
+
+  const register = async () => {
+    
+    // debugger;
+    try {
+      let response = await addUser(username, email, password)
+      setCurrentPage('Navigation')
+      setCurrentPage('Navigation')
+    } catch (error) {
+      let errMsg = JSON.parse(error.message).message
+      setRegisterFail(errMsg)
+    }
+  }
+
+
     return (
-      <div>
-          <h1>Sign up component</h1>
+      <div className="app flex-row align-items-center">
+          <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <CardGroup>
+                <Card className="p-2">
+                  <CardBody>
+                    <Form>
+                      
+                      <div
+                        className="totheRight"
+                        style={{  }}
+                      >
+                        <InputGroup className="mb-3">
+                          <Input
+                            type="text"
+                            onChange={(e)=>{changeUsername(e.target.value)}}
+                            placeholder="Enter username"
+                          />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                          <Input
+                            type="text"
+                            onChange={(e)=>{changeEmail(e.target.value)}}
+                            placeholder="Enter email"
+                          />
+                        </InputGroup>
+                        <InputGroup className="mb-4">
+                          <Input
+                            type="password"
+                            onChange={(e)=>{changePassword(e.target.value)}}
+                            placeholder="Enter password"
+                          />
+                          <br /><br />
+                        </InputGroup>
+                        {registerFail ? <div style={{ color: "red", fontSize: "1.5em", fontStyle: "italic" }}>{registerFail}<br /><br /></div> : <div></div>}
+                        
+                        <Button onClick={register} color="success" block>
+                          Create account
+                        </Button>
+                      </div>
+                    </Form>
+                    <br />
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
   
-  export default SignUp;
